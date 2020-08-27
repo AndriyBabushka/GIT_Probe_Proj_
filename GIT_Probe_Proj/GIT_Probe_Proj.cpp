85b3513d3202d7774ea9c2cd8b13f48ed4661c6a
AndriyBabushka <andriy.babushka@gmail.com>
2020-08-27 19:05:39 +0300

    new

--- GIT_Probe_Proj/GIT_Probe_Proj.cpp
+++ GIT_Probe_Proj/GIT_Probe_Proj.cpp
@@ -3,6 +3,8 @@
 int main()
 {
     std::cout << "Hello World!\n";
+    std::cout << "555555555555555555555555555\n";
+    std::cout << "777777777777777777777777777\n";
     std::cout << "dddddddddddddddddddddddddddd\n";
     std::cout << "hhhhhhhhhhhhhhhhhhhhhhh\n";
 
